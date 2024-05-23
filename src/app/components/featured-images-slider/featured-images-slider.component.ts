3dfba3117f137a01079d091de535821dddc05162
Fredrik Eklund <fredrik@bayville.se>
2024-05-23 08:19:09 +0200

    Added header, navigation, image slider

---
+++ src/app/components/featured-images-slider/featured-images-slider.component.ts
@@ -0,0 +1,37 @@
+import {Component, ElementRef, ViewChild } from "@angular/core"
+import KeenSlider, { KeenSliderInstance } from "keen-slider"
+import { NgOptimizedImage } from '@angular/common'
+
+
+
+@Component({
+  standalone: true,
+  selector: 'app-featured-images-slider',
+  templateUrl: './featured-images-slider.component.html',
+  styleUrls: ['../../../../node_modules/keen-slider/keen-slider.min.css','./featured-images-slider.component.scss', ],
+  imports: [NgOptimizedImage],
+})
+export class FeaturedImageSliderComponent {
+  @ViewChild("sliderRef") sliderRef!: ElementRef<HTMLElement>
+
+  slider!: KeenSliderInstance | null;
+
+  ngAfterViewInit() {
+    this.slider = new KeenSlider(this.sliderRef.nativeElement, {
+      breakpoints: {
+        "(min-width: 400px)": {
+          slides: { perView: 1, spacing: 0 },
+        },
+        "(min-width: 769px)": {
+          slides: { perView: 4, spacing: 0},
+          drag: false
+        },
+      },
+      slides: { perView: 1 },
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.slider) this.slider.destroy()
+  }
+}
---
+++ src/app/components/hero/hero.component.ts
@@ -0,0 +1,20 @@
+import { NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [NgIf, RouterLink],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.scss'
+})
+export class HeroComponent {
+  imgLink: string = '../../assets/img/hero';
+  imgLinkJPG: string = '../../assets/img/hero.jpg';
+  imgLinkAVIF: string = '../../assets/img/hero.jpg';
+  
+
+
+  }
